9ced549e2eca7fa64d808e9760781a1fafffacd5
Frankie <geos_tricks@outlook.com>
2023-03-17 13:45:20 +0800

    Delete buddy.rb

--- buddy.rb
+++
@@ -1,8 +0,0 @@
-# TODO: REturn your buddy's name
-# Make sure the method today_my_buddy's_github_nickname_is does return the Github username of your budyy.
-  # It is different than displaying it (puts). It should return a string!
-
-  def today_my_buddys_github_nickname_is
-    # TODO: Return the GitHub nickname of your buddy.
-    return "Frank82"
-  end
